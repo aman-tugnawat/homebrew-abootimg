e433b40f59801694dac6037f4a2a84c017b553c6
Aman Tugnawat <aman.tugnawat@gmail.com>
2026-03-22 18:03:41 -0400

    Fix formula version parameter

--- Formula/abootimg.rb
+++ Formula/abootimg.rb
@@ -2,7 +2,7 @@ class Abootimg < Formula
   desc "Tool to read/write/update android boot images"
   homepage "https://github.com/ggrandou/abootimg"
   url "https://github.com/ggrandou/abootimg/archive/7e127fee6a3981f6b0a50ce9910267cd501e09d4.tar.gz"
-  version "master"
+  version "0.8.1"
   sha256 "eb8d912bc7b5454c2afd3385fd86f4917d3587c48a6f5ae45df7856d88502cab"
   license "GPL-2.0-or-later"
 
