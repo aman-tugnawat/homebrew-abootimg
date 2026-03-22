8716756489c62222fdf330b0fb035ff2706e281c
Aman Tugnawat <aman.tugnawat@gmail.com>
2026-03-22 18:03:20 -0400

    Initial commit of abootimg formula

---
+++ Formula/abootimg.rb
@@ -0,0 +1,19 @@
+class Abootimg < Formula
+  desc "Tool to read/write/update android boot images"
+  homepage "https://github.com/ggrandou/abootimg"
+  url "https://github.com/ggrandou/abootimg/archive/7e127fee6a3981f6b0a50ce9910267cd501e09d4.tar.gz"
+  version "master"
+  sha256 "eb8d912bc7b5454c2afd3385fd86f4917d3587c48a6f5ae45df7856d88502cab"
+  license "GPL-2.0-or-later"
+
+  def install
+    system "make", "CPPFLAGS=", "LDLIBS="
+    bin.install "abootimg"
+    bin.install "abootimg-pack-initrd"
+    bin.install "abootimg-unpack-initrd"
+  end
+
+  test do
+    system "#{bin}/abootimg", "-h"
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-abootimg
+Homebrew tap for `abootimg`.
+
+## Installation
+```bash
+brew tap aman-tugnawat/abootimg
+brew install abootimg
+```
